b13bb76d3e94b14c349b76b65fd81269efb89192
Chandramani Vyas <chandramani09022001@gmail.com>
2020-10-26 18:36:06 +0530

    customer window

---
+++ customer.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from PIL import ImageTk,Image
+
+root = Tk()
+root.title("Pizza Corner!")
+root.geometry("800x500")
+root.iconbitmap('C:/Users/user/Documents/GitHub/Pizza-Corner/images/pizza.ico')
+
+myImg=ImageTk.PhotoImage(Image.open("C:/Users/user/Documents/GitHub/Pizza-Corner/images/Welll.png"))
+my_label =Label(root,image=myImg)
+my_label.pack(pady=10,padx=10)
+
+frame=LabelFrame(root,text='WELCOME!',font='Lora',padx=180,pady=50,bd=4)
+frame.pack()
+Order_Pizza=Button(frame, text='Order Pizza',height=2,width=14,bd=4,bg='#7ea04d',activebackground='#7ea04d')
+
+Cancel_Order=Button(frame, text='Cancel Order ',height=2,width=14,bd=4,bg='#7ea04d',activebackground='#7ea04d')
+Track_Order=Button(frame, text='Track Order ',height=2,width=14,bd=4,bg='#7ea04d',activebackground='#7ea04d')
+myLabel1=Label(frame,text="  ")
+myLabel2=Label(frame,text="  ")
+
+Order_Pizza.grid(row=0, column=0)
+myLabel1.grid(row=0,column=1)
+Cancel_Order.grid(row=0,column=2)
+myLabel2.grid(row=0,column=3)
+Track_Order.grid(row=0,column=4)
+
+
+
+
+
+root.mainloop()
