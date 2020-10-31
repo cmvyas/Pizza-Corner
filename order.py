79ed7b06b67fa85db6eb911fd682e19843702907
Chandramani Vyas <chandramani09022001@gmail.com>
2020-10-31 20:48:16 +0530

    variable

--- order.py
+++ order.py
@@ -12,36 +12,41 @@ my_label.pack(pady=2,padx=2)
 
 frame=LabelFrame(root,text='Order Your Pizza',font='Lora',padx=98,pady=12,bd=4)
 frame.pack()
-
+#name
 Name=Label(frame,text='Name',anchor=W).grid(row=0,column=0)
 free=Label(frame,text="   ").grid(row=0,column=1)
-n=Entry(frame).grid(row=0,column=2)
+name=StringVar()
+n=Entry(frame,textvariable="name").grid(row=0,column=2)
 freerow=Label(frame,text=' ').grid(row=1,column=0)
-
+#address
 Address=Label(frame,text='Address').grid(row=2,column=0)
 free1=Label(frame,text="   ").grid(row=2,column=1)
-a=Entry(frame).grid(row=2,column=2)
+address=StringVar()
+a=Entry(frame,textvariable='address').grid(row=2,column=2)
 freerow2=Label(frame,text=" ").grid(row=3,column=0)
-
+#type
 PizzaType=Label(frame,text="Pizza Type").grid(row=4,column=0)
 free2=Label(frame,text=" ").grid(row=4,column=1)
-small= Checkbutton(frame,text='Small').grid(row=4,column=2)
-Medium= Checkbutton(frame,text='Medium').grid(row=4,column=3)
-Large= Checkbutton(frame,text='Large').grid(row=4,column=4)
+p_type=StringVar()
+small= Radiobutton(frame,text='Small',variable= p_type,value="Small").grid(row=4,column=2)
+Medium= Radiobutton(frame,text='Medium',variable=p_type,value="Medium").grid(row=4,column=3)
+Large= Radiobutton(frame,text='Large',variable=p_type,value="Large").grid(row=4,column=4)
 freerow3=Label(frame,text=" ").grid(row=5,column=0)
-
+#mobile
 Mobile=Label(frame,text='Mobile No.').grid(row=6,column=0)
 free3=Label(frame,text="   ").grid(row=6,column=1)
-m=Entry(frame).grid(row=6,column=2)
+mobile=StringVar()
+m=Entry(frame,textvariable="mobile").grid(row=6,column=2)
 freerow4=Label(frame,text="").grid(row=7,column=0)
-
+#email
 Email=Label(frame,text='E-mail').grid(row=8,column=0)
-free4=Label(frame,text="   ").grid(row=8,column=1)
-e=Entry(frame).grid(row=8,column=2)
+free4=Label(frame,text="").grid(row=8,column=1)
+email=StringVar()
+e=Entry(frame,textvariable='email').grid(row=8,column=2)
 freerow5=Label(frame,text="").grid(row=9,column=0)
-
 free5=Label(frame,text="").grid(row=10,column=0)
 free5=Label(frame,text="").grid(row=10,column=1)
+#button
 def order():
     response= messagebox.showinfo("Order","Your order will be served soon")
 
